f700f3cdf467d25170a3d9112fc54143e4006366
YuriTadevosyan96 <78906093+YuriTadevosyan96@users.noreply.github.com>
2021-07-23 02:08:06 +0300

    Add game full logic

---
+++ src/components/GameOver/GameOver.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './GameOver.module.css';
+
+function GameOver() {
+  return <h2 className={classes.GameOver}>GAME OVER</h2>;
+}
+
+export default GameOver;
