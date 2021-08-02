65654381051a9cc8fa9bb7efc0b76d784d6391e4
YuriTadevosyan96 <78906093+YuriTadevosyan96@users.noreply.github.com>
2021-08-03 00:27:20 +0300

    Bugfix: fixed ever incresing maximum score

--- src/components/GameBoardCell/GameBoardCell.js
+++ src/components/GameBoardCell/GameBoardCell.js
@@ -2,14 +2,15 @@ import React from 'react';
 
 import classes from './GameBoardCell.module.css';
 
-const GameBoardCell = ({ isFoodCell, isSnakeBodyPart }) => {
+const GameBoardCell = ({ isFoodCell, isSnakeBodyPart, isSnakeHead }) => {
   const appliedClasses = [classes.GameBoardCell];
-  if (isFoodCell) {
-    appliedClasses.push(classes.FoodCell);
+  if (isSnakeHead) {
+    appliedClasses.push(classes.SnakeHead);
   } else if (isSnakeBodyPart) {
     appliedClasses.push(classes.SnakeBodyPart);
+  } else if (isFoodCell) {
+    appliedClasses.push(classes.FoodCell);
   }
-
   return <div className={appliedClasses.join(' ')}></div>;
 };
 
